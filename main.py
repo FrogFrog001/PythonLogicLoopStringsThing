ba20fb15b48e72dc558807bc3058d19a74a67f3c
frog <jeffreylam001@gmail.com>
2018-12-18 22:20:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+#OOF
+text = input("Enter in a sentence: ")
+
+numbertest = 0
+
+#Splits the text at the space
+splittext = (text.split())
+
+print(splittext)
+
+for item in splittext:
+    numbertest = numbertest + 1
+    print(numbertest)
+print("terms")
+reoccuringtimes = 0
+mostusedterm1 = 0
+mostusedterm2 = 0
+mostusedterm3 = 0
+mostusedterm4 = 0
+mostusedterm5 = 0
+i2 = 0
+numOfItems = numbertest
+yeet = numOfItems + 1
+
+#trippy logic loop
+#while yeet <= numbertest:
+#	  i1 = i2
+#	  a = (text.partition(' ')[i1])
+# 	i2 = i1 + 1
+#	  b = (text.partition(' ')[yeet])
+#	  if a == b:
+#	     reoccuringtimes = reoccuringtimes + 1
+#	
+#	i2 = i2 + 1
+#print(reoccuringtimes)
+	    
+	
+	
+
